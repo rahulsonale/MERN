9d6828caf5bb06180acb04119c9504ccf6f40d20
Rahulsonale <rahulsonale002@gmail.com>
2026-01-08 12:35:56 +0530

    four chamber ques

--- Conditions.js
+++ Conditions.js
@@ -12,3 +12,25 @@ function num(n) {
   }
 }
 console.log(num(n));
+
+/*Quadrants
+Input
+
+10
+6
+Output
+1*/
+function chamber(x, y) {
+  if (x > 0 && y > 0) {
+    console.log(1);
+  } else if (x < 0 && y < 0) {
+    console.log(3);
+  } else if (x > 0 && y < 0) {
+    console.log(4);
+  } else if (x < 0 && y > 0) {
+    console.log(2);
+  } else {
+    console.log("none");
+  }
+}
+chamber(x, y);
