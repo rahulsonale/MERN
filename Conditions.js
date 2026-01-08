2b26506098ffd25b9ffc635208361dc32ae767e2
Rahulsonale <rahulsonale002@gmail.com>
2026-01-08 12:30:01 +0530

    conditon ques

---
+++ Conditions.js
@@ -0,0 +1,14 @@
+/*Number Comparison
+Input
+
+1
+Output
+You entered less*/
+function num(n) {
+  if (n > 1) {
+    return "You entered more";
+  } else {
+    return "You entered less";
+  }
+}
+console.log(num(n));
