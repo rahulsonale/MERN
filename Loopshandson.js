3a93ad8ba1760ee65d6af51b939846ba46831b96
Rahulsonale <rahulsonale002@gmail.com>
2026-01-11 23:52:52 +0530

    N Stars

--- Loopshandson.js
+++ Loopshandson.js
@@ -8,3 +8,39 @@ for (i = 1; i <= n; i++) {
   }
 }
 console.log(ans);
+
+/*Palindrome Number Checker*/
+let reverse = 0;
+let original = Number(n);
+while (n > 0) {
+  let rev = n % 10;
+  reverse = reverse * 10 + rev;
+  n = Math.floor(n / 10);
+}
+
+if (reverse === original) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+/*N Stars
+Input
+3
+output:
+----------
+* * *    -
+*        -
+*        -
+*        -
+----------
+*/
+let row = "";
+for (let i = 1; i <= n; i++) {
+  row += "* ";
+}
+console.log(row);
+
+for (let j = 1; j <= n; j++) {
+  console.log("*");
+}
