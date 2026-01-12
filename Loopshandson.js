a5e6c3944d680e2d82e0211a2ee61853eb57cac8
Rahulsonale <rahulsonale002@gmail.com>
2026-01-12 13:05:22 +0530

    Star Pyramid Creation

--- Loopshandson.js
+++ Loopshandson.js
@@ -82,3 +82,23 @@ function is_prime(N) {
   return `${N} is a prime number`;
 }
 //-->Call function here
+
+/*ques:Star Pyramid Creation
+Pattern Image
+  *
+ * *
+* * *
+
+*/
+for (let i = 1; i <= n; i++) {
+  let row = "";
+  for (let j = 1; j <= n - i; j++) {
+    row += " ";
+  }
+  for (let k = 1; k <= i; k++) {
+    row += "* ";
+  }
+  console.log(row);
+}
+
+//-->n = input
