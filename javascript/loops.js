1cbe1fafb9552b8c0da5bbb87c57244997419fa3
Rahulsonale <rahulsonale002@gmail.com>
2026-02-01 20:15:26 +0530

    Descending number pattern

--- javascript/loops.js
+++ javascript/loops.js
@@ -277,3 +277,12 @@ for (let i = 1; i <= n; i++) {
 for (i = 1; i <= n; i++) {
   console.log("*".repeat(i));
 }
+
+//Descending Number Pattern
+for (let i = 1; i <= n; i++) {
+  let row = "";
+  for (let j = 1; j >= 1; j--) {
+    row += j;
+  }
+  console.log(row);
+}
