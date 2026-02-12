a4eb2cb0e5f4f0f10b5cb6c7305f9bac7fbeb63e
Rahulsonale <rahulsonale002@gmail.com>
2026-02-12 20:54:51 +0530

    fetch api

--- Asynchronous/4.Fetch API and real API/main.js
+++ Asynchronous/4.Fetch API and real API/main.js
@@ -22,8 +22,8 @@ fetch("https://jsonplaceholder.typicode.com/posts")
   .then((response) => response.json())
   .then((result) => console.log(result));
 
-function showLoader() {
-  document.getElementById("status").textContent = "loading...";
+function showLoader(content = "loading...") {
+  document.getElementById("status").textContent = content;
 }
 function clearLoader() {
   document.getElementById("status").textContent = "";
@@ -39,17 +39,20 @@ function fetchPosts(postId) {
 //     return fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`).then(response => {response.json()})
 // }
 function fetchComments(postId) {
+  showLoader("fetching comments...");
   return fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`,
   ).then((response) => response.json());
 }
 
 function fetchUsers(comments) {
+  showLoader("fetching user...");
   return fetch("https://jsonplaceholder.typicode.com/users")
     .then((response) => response.json())
     .then((users) => {
       return comments.map((comment) => {
         // users.find(user=> user.email === comment.email)
+        // using random index to attach a user with comment
         return {
           ...comment,
           user: users[Math.floor(Math.random() * users.length)],
@@ -74,6 +77,7 @@ function showComments(comments) {
 }
 
 function showPosts(post) {
+  showLoader("fetching posts....");
   const postElement = document.getElementById("post");
   postElement.innerHTML = `<article id=${post.id}>
         <h1>${post.title}</h1>
