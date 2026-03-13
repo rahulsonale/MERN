d093c52391c28bfeabcb23446e224fd7ff1ac0dc
Rahulsonale <rahulsonale002@gmail.com>
2026-03-13 13:00:24 +0530

    CRUD

--- React/9.CRUD operations with express/server.mjs
+++ React/9.CRUD operations with express/server.mjs
@@ -1,34 +1,14 @@
 import express from "express";
 import todosRouter from "./todos.router.mjs";
-import { todos } from "./db.mjs";
-import crypto from "crypto";
 
 const app = express();
-app.use(express.json());
-
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Server is working , Home page");
+  res.send("Server is working, Home page");
 });
-
-app.post("/todos ", (req, res) => {
-  const { title } = req.body;
-  const createdTodo = createNewTodo(title);
-  res.status(200).send(createdTodo);
-});
-
-function createNewTodo(title) {
-  const newTodo = {
-    id: crypto.randomUUID(),
-    title,
-    completed: false,
-    createDate: new Date().toISOString(),
-  };
-
-  todos.push(newTodo);
-  return newTodo;
-}
 
 app.use("/todos", todosRouter);
 
--- React/9.CRUD operations with express/todos.router.mjs
+++ React/9.CRUD operations with express/todos.router.mjs
@@ -1,10 +1,61 @@
-import { Router } from "express";
-const router = Router();
-
+import { Router, json } from "express";
 import { todos } from "./db.mjs";
 
-router.get("/", (req, res) => {
-  res.json(todos);
-});
+const router = Router();
+router.use(json());
+router
+  .get("/", (req, res) => {
+    res.json(todos);
+  })
+  .post("/", (req, res) => {
+    try {
+      console.log("body", req.body);
+      if (!req.body.title) {
+        res.status(400).send({ message: "Title is required" });
+      } else {
+        console.log("post called");
+        console.log(req.body);
+        const { title } = req.body;
+        const createdTodo = createNewTodo(title);
+        res.status(201).send(createdTodo);
+      }
+    } catch (ex) {
+      res.status(500).send({ message: "Error while saving todo", error: ex });
+    }
+  })
+  .patch("/:id", (req, res) => {
+    try {
+      const updatedTodoId = updateTodo(req.params.id, req.body);
+      if (!updatedTodoId) {
+        res.status(404).send({ message: "todo not found" });
+      }
+      res.send(updatedTodoId);
+    } catch (ex) {
+      res.status(500).send({ message: ex });
+    }
+  })
+  .delete("/:id", (req, res) => {
+    // HW: deleting a todo
+    deleteTodo();
+  });
+
+function deleteTodo(todoId) {}
+
+function updateTodo(todoId, updatedTodo) {
+  let existingTodoIndex = todos.findIndex((todo) => todo.id === todoId);
+  todos[existingTodoIndex] = { ...todos[existingTodoIndex], ...updatedTodo };
+  return todos[existingTodoIndex].id;
+}
+
+function createNewTodo(title) {
+  const newTodo = {
+    id: crypto.randomUUID(),
+    title,
+    completed: false,
+    createDate: new Date().toISOString(),
+  };
+  todos.push(newTodo);
+  return newTodo;
+}
 
 export default router;
