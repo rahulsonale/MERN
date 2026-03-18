6328f82adcdc7551a94aa849c881c4fda7a1621b
Rahulsonale <rahulsonale002@gmail.com>
2026-03-18 10:11:21 +0530

    NotesApp

---
+++ React/12.Mongodb/db.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+//Mongoclient is the class used to connect node.js to mongodb
+//ServerAPiversion : This is used to specify mongodb api version for stability.
+const uri = process.env.CONNECTION_STRING;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
